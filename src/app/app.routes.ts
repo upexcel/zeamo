dfbe9d4d109fca0d9482e9ce4eebd1ab2f1f46b0
etech <excellencetechnologies>
2016-09-23 11:44:49 +0530

    home design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,8 @@
 import { Routes, RouterModule } from '@angular/router';
-import { Home } from './home';
-import { About } from './about';
-import { NoContent } from './no-content';
-
-import { DataResolver } from './app.resolver';
+import { HomePage } from './home';
 
 
 export const ROUTES: Routes = [
-  { path: '',      component: Home },
-  { path: 'home',  component: Home },
-  { path: 'about', component: About },
-  {
-    path: 'detail', loadChildren: () => System.import('./+detail')
-  },
-  { path: '**',    component: NoContent },
+  { path: '',  component: HomePage },
+  { path: '**',    component: HomePage },
 ];
